929bde27b0f6299d20bf9408826b828664ac0d2a
Shahar Bar Or <shaharbaror1@gmail.com>
2025-01-27 17:55:40 +0200

    starting to make navigation

---
+++ My project/Assets/Scripts/InterSections.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class InterSections : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // get all the connected intersections
+    public InterSections[] sections;
+    protected float x;
+    protected float y;
+    void Start()
+    {
+        x = transform.position.x;
+        y = transform.position.y;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
